e8f5593edda51d2eedea85cfbb63c36e5101a4f0
SpitaleA <agustin.spitale@nybblegroup.com>
2022-10-18 12:36:17 -0300

    Pongo sleeps

--- src/test/java/basicSelenium/CRUDProjectTest.java
+++ src/test/java/basicSelenium/CRUDProjectTest.java
@@ -24,11 +24,10 @@ public class CRUDProjectTest {
         // set email
         driver.findElement(By.id("ctl00_MainContent_LoginControl1_TextBoxEmail")).sendKeys("agusto15@nybble.com");
         // set pwd
-        driver.findElement(By.id("ctl00_MainContent_LoginControl1_TextBoxPassword")).sendKeys("12345");
+        driver.findElement(By.id("ctl00_MainContent_LoginControl1_TextBoxPassword")).sendKeys("123456");
         // click login
         driver.findElement(By.id("ctl00_MainContent_LoginControl1_ButtonLogin")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-//        Thread.sleep(2000);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
     @AfterEach
     public void cleanUp(){
@@ -37,55 +36,54 @@ public class CRUDProjectTest {
 
 
     @Test
-    public void createProject() {
-//        Thread.sleep(1000);
+    public void createProject() throws InterruptedException {
         int largo = driver.findElements(By.xpath("//li//td[text()='nuevo projecto update']")).size();
         int largo2 = driver.findElements(By.xpath("//li//td[text()='nuevo projecto']")).size();
 
         //Boton add new project
         driver.findElement(By.cssSelector("#MainTable > tbody > tr > td.MainTableLeft > div:nth-child(6) > div")).click();
-//        Thread.sleep(1000);
+        Thread.sleep(1000);
         driver.findElement(By.id("NewProjNameInput")).sendKeys("nuevo projecto");
         driver.findElement(By.id("NewProjNameButton")).click();
-//        Thread.sleep(2000);
+        Thread.sleep(2000);
 
         //Verificacion
         Assertions.assertEquals(largo2+1,driver.findElements(By.xpath("//li//td[text()='nuevo " +
                 "projecto']")).size(),"ERROR no se creo el proyecto");
-//        Thread.sleep(1000);
+        Thread.sleep(1000);
 
         //Edit project
         driver.findElement(By.xpath("//li[last()]//td[text()='nuevo projecto']")).click();
-//        Thread.sleep(1000);
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//li[last()]//td[text()='nuevo projecto']//following-sibling::td//div[@style=\"display: block;\"]//img[@src=\"/Images/dropdown.png\"]")).click();
-//        Thread.sleep(1000);
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@id=\"projectContextMenu\"]/li[1]/a")).click();
-//        Thread.sleep(1000);
+        Thread.sleep(1000);
         driver.findElement(By.id("ItemEditTextbox")).sendKeys(" update");
-//        Thread.sleep(1000);
+        Thread.sleep(1000);
         driver.findElement(By.id("ItemEditSubmit")).click();
-//        Thread.sleep(1000);
+        Thread.sleep(1000);
 
         //Verificacion
         Assertions.assertEquals(largo+1,driver.findElements(By.xpath("//li//td[text()='nuevo " +
                 "projecto update']")).size(),"ERROR no se le cambio el nombre al proyecto");
-//        Thread.sleep(1000);
+        Thread.sleep(1000);
 
 
         //Delete
-//        Thread.sleep(1000);
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//li[last()]//td[text()='nuevo projecto update']")).click();
-//        Thread.sleep(1000);
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//li[last()]//td[text()='nuevo projecto update']//following-sibling::td//div[@style=\"display: block;\"]//img[@src=\"/Images/dropdown.png\"]")).click();
-//        Thread.sleep(1000);
+        Thread.sleep(1000);
         driver.findElement(By.id("ProjShareMenuDel")).click();
-//        Thread.sleep(1000);
+        Thread.sleep(1000);
         driver.switchTo().alert().accept();
-//        Thread.sleep(1000);
+        Thread.sleep(1000);
 
         //Verificacion
         Assertions.assertEquals(largo,driver.findElements(By.xpath("//li//td[text()='nuevo " +
                 "projecto update']")).size(),"ERROR no se borro el elemento");
-//        Thread.sleep(1000);
+        Thread.sleep(1000);
     }
 }
--- src/test/java/basicSelenium/Register.java
+++ src/test/java/basicSelenium/Register.java
@@ -1,4 +1,88 @@
 package basicSelenium;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.nio.charset.Charset;
+import java.time.Duration;
+import java.util.Random;
+
 public class Register {
+
+    public String email = getAlphaNumericString()+"@"+getAlphaNumericString()+".com";
+    WebDriver driver;
+    @BeforeEach
+    public void setup() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","src/test/resources/driver/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://todo.ly/");
+
+
+        // click sing up
+        driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_PanelNotAuth\"]/div[2]/div[1]/div[3]/a")).click();
+        // set name
+        driver.findElement(By.id("ctl00_MainContent_SignupControl1_TextBoxFullName")).sendKeys("agusto");
+        // set email
+        driver.findElement(By.id("ctl00_MainContent_SignupControl1_TextBoxEmail")).sendKeys(email);
+        // set pwd
+        driver.findElement(By.id("ctl00_MainContent_SignupControl1_TextBoxPassword")).sendKeys("12345");
+        //click on checkbox
+        driver.findElement(By.id("ctl00_MainContent_SignupControl1_CheckBoxTerms")).click();
+        // click sing up
+        driver.findElement(By.id("ctl00_MainContent_SignupControl1_ButtonSignup")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @AfterEach
+    public void cleanUp(){
+        driver.quit();
+    }
+    @Test
+    public void changePWD() throws InterruptedException {
+        driver.findElement(By.xpath("//*[@id=\"ctl00_HeaderTopControl1_PanelHeaderButtons\"]/a[1]")).click();
+        driver.findElement(By.id("TextPwOld")).sendKeys("12345");
+        driver.findElement(By.id("TextPwNew")).sendKeys("123456");
+        driver.findElement(By.xpath("/html/body/div[9]/div[2]/div/button[1]/span")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.id("ctl00_HeaderTopControl1_LinkButtonLogout")).click();
+
+        // click login
+        driver.findElement(By.xpath("//img[@src='/Images/design/pagelogin.png']")).click();
+        // set email
+        driver.findElement(By.id("ctl00_MainContent_LoginControl1_TextBoxEmail")).sendKeys(email);
+        // set pwd
+        driver.findElement(By.id("ctl00_MainContent_LoginControl1_TextBoxPassword")).sendKeys("123456");
+        // click login
+        driver.findElement(By.id("ctl00_MainContent_LoginControl1_ButtonLogin")).click();
+        Thread.sleep(1000);
+    }
+    static String getAlphaNumericString()
+    {
+
+        // chose a Character random from this String
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "0123456789"
+                + "abcdefghijklmnopqrstuvxyz";
+
+        // create StringBuffer size of AlphaNumericString
+        StringBuilder sb = new StringBuilder(6);
+
+        for (int i = 0; i < 6; i++) {
+
+            // generate a random number between
+            // 0 to AlphaNumericString variable length
+            int index
+                    = (int)(AlphaNumericString.length()
+                    * Math.random());
+
+            // add Character one by one in end of sb
+            sb.append(AlphaNumericString
+                    .charAt(index));
+        }
+
+        return sb.toString();
+    }
 }
