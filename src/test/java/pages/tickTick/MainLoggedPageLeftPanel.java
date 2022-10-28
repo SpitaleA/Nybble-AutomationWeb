af13a49b3450333043e05e4b13b67c614120b457
SpitaleA <agustin.spitale@nybblegroup.com>
2022-10-28 13:10:31 -0300

    Examen ejercicio 4

--- src/test/java/pages/tickTick/MainLoggedPageLeftPanel.java
+++ src/test/java/pages/tickTick/MainLoggedPageLeftPanel.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
 public class MainLoggedPageLeftPanel {
     public Button listsButton = new Button(By.xpath("//ul[@id=\"project-ul\"]//p[contains(@class,\"text-s\")]"));
 
-    public Button addNewListBtn = new Button(By.xpath("(//*[contains(@class,\"icon-list-add\")])[1]"));
+    public Button addNewListBtn = new Button(By.xpath("//p[text()=\"Lists\"]/following-sibling::button//*[contains(@class,\"icon-list-add\")]"));
     public Button searchListByname(String name){
         try {
             Button listBtn = new Button(
